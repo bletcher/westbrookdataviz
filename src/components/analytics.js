0dc3a51386f48390e140fa2f0c1f1424a20d26d2
Ben Letcher <bletcher@usgs.gov>
2025-04-24 21:57:49 -0400

    put analytics code in index.md

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -1,19 +1,20 @@
 export function setupAnalytics() {
-  // Create the script element with AWS RUM configuration
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://rum.us-east-2.amazonaws.com/1.0.2/cwr.js';
-  script.onload = function() {
-    window.cwr('init', {
-      sessionSampleRate: 1,
-      identityPoolId: "us-east-2:ace87ca1-6eaa-4f02-9b0e-cf9ac1a67014",
-      endpoint: "https://dataplane.rum.us-east-2.amazonaws.com",
-      telemetries: ["performance", "errors", "http"],
-      allowCookies: true,
-      enableXRay: false
-    });
-  };
-  
-  // Add the script to the document head
-  document.head.insertBefore(script, document.head.firstChild);
+  try {
+    const script = document.createElement('script');
+    script.src = 'https://client.rum.us-east-2.amazonaws.com/1.0.2/cwr.js';
+    script.async = true;
+    script.crossOrigin = 'anonymous';
+    script.onload = function() {
+      window.cwr('init', {
+        sessionSampleRate: 1,
+        identityPoolId: 'us-east-2:ace87ca1-6eaa-4f02-9b0e-cf9ac1a67014',
+        endpoint: 'https://dataplane.rum.us-east-2.amazonaws.com',
+        telemetries: ['errors', 'performance'],
+        allowCookies: true
+      });
+    };
+    document.head.appendChild(script);
+  } catch (error) {
+    console.error('Error initializing AWS RUM:', error);
+  }
 } 
