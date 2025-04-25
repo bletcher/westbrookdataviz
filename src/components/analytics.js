749ac66d669fc27c9b5cc7345f81d5fabac1f27e
Ben Letcher <bletcher@usgs.gov>
2025-04-24 21:35:40 -0400

    update analytics

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -1,28 +1,19 @@
-import { AwsRum } from 'aws-rum-web';
-
 export function setupAnalytics() {
-  try {
-    const config = {
+  // Create the script element with AWS RUM configuration
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://rum.us-east-2.amazonaws.com/1.0.2/cwr.js';
+  script.onload = function() {
+    window.cwr('init', {
       sessionSampleRate: 1,
       identityPoolId: "us-east-2:ace87ca1-6eaa-4f02-9b0e-cf9ac1a67014",
       endpoint: "https://dataplane.rum.us-east-2.amazonaws.com",
       telemetries: ["performance", "errors", "http"],
       allowCookies: true,
       enableXRay: false
-    };
-
-    const APPLICATION_ID = '25e7c184-e75c-43a2-93b4-5d571327cbd9';
-    const APPLICATION_VERSION = '1.0.0';
-    const APPLICATION_REGION = 'us-east-2';
-
-    const awsRum = new AwsRum(
-      APPLICATION_ID,
-      APPLICATION_VERSION,
-      APPLICATION_REGION,
-      config
-    );
-  } catch (error) {
-    // Ignore errors thrown during CloudWatch RUM web client initialization
-    console.error('Failed to initialize AWS RUM:', error);
-  }
+    });
+  };
+  
+  // Add the script to the document head
+  document.head.insertBefore(script, document.head.firstChild);
 } 
